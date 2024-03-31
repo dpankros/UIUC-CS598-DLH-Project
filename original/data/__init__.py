2dd0088b532c9975f03bc17158549ff7d3986e2d
Aaron Schlesinger <aaron@ecomaz.net>
2024-03-31 14:41:53 -0700

    adding conda environment file and comments

--- original/data/__init__.py
+++ original/data/__init__.py
@@ -1,3 +1,11 @@
-def add_noise_to_data(a):
+def add_noise_to_data[T](a: T) -> T:
+    """
+    Stand-in function for adding noise to data.
+
+    This function is used in the codebase, hidden behind a
+    config flag, and not defined anywhere in the original research
+    repository, so we have defined it here as the identity function,
+    just so things run properly.
+    """
     print("WARNING: 'test_noise_snr' config option is a no-op")
     return a
