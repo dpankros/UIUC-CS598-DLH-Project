de0faf76d877df03ff6894b306927b374b0fb9e3
Aaron Schlesinger <aaron@ecomaz.net>
2024-04-06 18:56:12 +0000

    fixing issues with num folds and channel selection

--- original/train.py
+++ original/train.py
@@ -4,49 +4,105 @@ import numpy as np
 from keras.callbacks import LearningRateScheduler, EarlyStopping
 from keras.losses import BinaryCrossentropy
 from sklearn.utils import shuffle
+import numpy.typing as npt
 
 from models.models import get_model
 
 THRESHOLD = 1
 FOLD = 5
 
-
 def lr_schedule(epoch, lr):
     if epoch > 50 and (epoch - 1) % 5 == 0:
         lr *= 0.5
     return lr
 
+def _replace_final_dim(orig_x: npt.NDArray, final_dim_size: int) -> npt.NDArray:
+    '''
+    Given an NDArray `orig_x`, return a new NDArray of the same shape,
+    except with final dimension `final_dim_size`.
+    '''
+    orig_x_shape = orig_x.shape
+    x_transform = np.zeros(
+        (*orig_x_shape[:-1], final_dim_size)
+    )
+    assert x_transform.shape[:-1] == orig_x.shape[:-1]
+    return x_transform
 
-def train(config, fold=None):
+
+def train(config, fold: int | None = None):
+    print(f'training with config {config}, fold={fold}')
+
     data = np.load(config["data_path"], allow_pickle=True)
 
     x, y_apnea, y_hypopnea = data['x'], data['y_apnea'], data['y_hypopnea']
-    print(x, y_apnea, y_hypopnea)
+    print(
+        f'x={x.shape}, y_apnea={y_apnea.shape}, y_hypopnea={y_hypopnea.shape}'
+    )
     y = y_apnea + y_hypopnea
     ########################################################################################
-    for i in range(FOLD):
-        x[i], y[i] = shuffle(x[i], y[i])
+    # Channel selection
+    # 
+    # x has a shape similar to (NUM_FOLDS, 530, 1920, NUM_CHANNELS).
+    # each fold is thus shape (530, 1920, NUM_CHANNELS), but we're trying
+    # to only extract config["channels"] out into the last dimension.
+    # 
+    # The new x_transform ndarray is the same shape as the original x, except
+    # that its last dimension is the number of channels we're trying to 
+    # extract. Since the channels we want to extract comes from configuration,
+    # we can't guarantee we'll get the same number of channels as the size 
+    # of the final dimension of x, so we have to create a transformed x with
+    # the right size in the final dimension.
+    chans = config['channels']
+    assert (len(x.shape)) == 4
+    x_transform = _replace_final_dim(x, len(chans))
+    
+    
+    print(f'Extracting channels {chans}')
+    max_fold = min(FOLD, x_transform.shape[0])
+    if max_fold < x_transform.shape[0]:
+        print(
+            f'WARNING: only looking at the first {max_fold} of '
+            f'{x_transform.shape[0]} total folds in X'
+        )
+    # for i in range(FOLD):
+    for i in range(max_fold):
+        x_transform[i], y[i] = shuffle(x_transform[i], y[i])
         x[i] = np.nan_to_num(x[i], nan=-1)
         if config["regression"]:
             y[i] = np.sqrt(y[i])
             y[i][y[i] != 0] += 2
         else:
             y[i] = np.where(y[i] >= THRESHOLD, 1, 0)
-
-        x[i] = x[i][:, :, config["channels"]]  # CHANNEL SELECTION
+        
+        replace = x[i][:, :, chans]
+        print(
+            f'x[{i}].shape = {x[i].shape}, '
+            f'assigning x_transform[{i}] to {replace.shape}'
+        )
+        
+        x_transform[i] = replace  # CHANNEL SELECTION
 
     ########################################################################################
-    folds = range(FOLD) if fold is None else [fold]
+    # we want to select either `fold` if it's not none, and otherwise `FOLD`
+    # after we do that selection, take the min of either that or the size
+    # of x_transform's first dimension. we'll end up with a number no more 
+    # than the number of available folds in x_transform.
+    # folds = range(FOLD) if fold is None else range(fold)
+    num_folds = min(
+        FOLD if fold is None else fold,
+        x_transform.shape[0]
+    )
+    folds = range(num_folds)
     for fold in folds:
         first = True
         for i in range(5):
             if i != fold:
                 if first:
-                    x_train = x[i]
+                    x_train = x_transform[i]
                     y_train = y[i]
                     first = False
                 else:
-                    x_train = np.concatenate((x_train, x[i]))
+                    x_train = np.concatenate((x_train, x_transform[i]))
                     y_train = np.concatenate((y_train, y[i]))
 
         model = get_model(config)
