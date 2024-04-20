08b3ddc1126dbd0fbeaba27705b9b8f5e20742c2
Aaron Schlesinger <aaron@ecomaz.net>
2024-04-20 22:16:13 +0000

    cleaning up configs for training
    Signed-off-by: Aaron Schlesinger <aaron@ecomaz.net>

--- original/main_nch.py
+++ original/main_nch.py
@@ -3,6 +3,7 @@ import itertools
 import os
 
 from test import test
+from config.train import ModelPrefix, parse_train_env
 from train import train
 from model_name import get_model_name
 
@@ -50,7 +51,7 @@ EXCLUDED_SIGS = ["DEMO"]
 ALLOWED_LENGTHS = [3, 4, 5]
 
 
-def all_combinations(signal_names: list, lengths: (list | None) = None) -> list:
+def all_combinations(signal_names: list[str], lengths: (list[int] | None) = None) -> list:
     """
     Return all the combinations of signals from the list of signal_names where the number of signals is listed in lengths
     :param signal_names: a list of all possible signal names
@@ -66,47 +67,16 @@ def all_combinations(signal_names: list, lengths: (list | None) = None) -> list:
 
     return all
 
-
-def getenv_bool(key_name: str, default: bool) -> bool:
-    ret_str = os.getenv(key_name, str(default))
-    return True if ret_str == "True" else False
-
-
 if __name__ == "__main__":
     if RUN_ALL_COMBINATIONS:
         sig_names = [*filter(lambda n: n not in EXCLUDED_SIGS, sig_dict.keys())]
         # override the selected list of channels with all the combinations, except those in EXCLUDED_SIGS
         channel_list = all_combinations(sig_names, ALLOWED_LENGTHS)
 
-    data_root = os.getenv(
-        "DLHPROJ_DATA_ROOT",
-        "/mnt/e/data"
-    )
-    model_path = os.getenv(
-        "DLHPROJ_MODEL_PATH",
-        # "./weights/semscnn_ecgspo2/f"
-        None
-    )
-    model_dir = os.getenv(
-        "DLHPROJ_MODEL_DIR",
-        "./weights"
-    )
-
-    n_epochs = int(os.getenv(
-        "DLHPROJ_NUM_EPOCHS",
-        "100"
-    ))
-    force_retrain = getenv_bool(
-        key_name="DLHPROJ_FORCE_RETRAIN",
-        default=False,
-    )
+    model_env = parse_train_env()
     print(
         f"-----beginning training-----\n"
-        f"data_root={data_root}\n"
-        f"model_path={model_path}\n"
-        f"model_dir={model_dir}\n"
-        f"num_epochs={n_epochs}\n"
-        f"force_retrain={force_retrain}\n"
+        f"model_env={model_env}"
         "----------"
     )
     print(
@@ -119,39 +89,29 @@ if __name__ == "__main__":
     print("----------\n")
 
     for n, ch in enumerate(channel_list):
-        chs = []
+        chs: list[float] = []
         chstr = ""
         for name in ch:
             chstr += name
             chs = chs + sig_dict[name]
-        config = {
-            "data_path": f"{data_root}/nch_30x64.npz",
-            "model_path": f"{model_path}" if model_path is not None else None,
-            "model_dir": f"{model_dir}",
-            # "model_name": "sem-mscnn_" + chstr,  # Must be one of: "Transformer", "cnn", "sem-mscnn", "cnn-lstm", "hybrid"
-            "model_name": "Transformer_" + chstr,
-            # Must be one of: Transformer: "cnn", "sem-mscnn", "cnn-lstm", "hybrid"
-            "regression": False,
-            "transformer_layers": 5,  # best 5
-            "drop_out_rate": 0.25,  # best 0.25
-            "num_patches": 30,  # best 30 TBD
-            "transformer_units": 32,  # best 32
-            "regularization_weight": 0.001,  # best 0.001
-            "num_heads": 4,
-            "epochs": n_epochs,  # best 200
-            "channels": chs,
-        }
+        
+        train = model_env.to_train_config(
+            ModelPrefix.Transformer,
+            chstr,
+            chs,
+        )
+
         print(
             f"---{n + 1} of {len(channel_list)}----\n"
-            f"model_name={model_path if model_path else get_model_name(config)}\n"
+            f"model_name={model_env.model_path if model_env.model_path else get_model_name(train)}\n"
             f"training channel {chstr}..."
         )
-        train(config=config, force_retrain=force_retrain)
+        train(config=train, force_retrain=model_env.force_retrain)
         print(
             f"\ndone training. beginning testing...\n"
             f"----------\n"
         )
-        test(config)
+        test(train)
         print(
             f'done testing\n'
             '----------'
