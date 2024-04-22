4ccd2c629098a175c15de718866510441168ee2c
Aaron Schlesinger <aaron@ecomaz.net>
2024-04-22 00:11:35 +0000

    more docs
    Signed-off-by: Aaron Schlesinger <aaron@ecomaz.net>

--- original/eval/__init__.py
+++ original/eval/__init__.py
@@ -10,10 +10,17 @@ INDIVIDUAL_CHANS = set([
 def sorted_chan_str(chan_str: str) -> str:
     """
     Given a string composed exclusively of elements of INDIVIDUAL_CHANS,
-    return a new string with the exact same elements sorted alphabetically
+    return a new string with the exact same elements sorted alphabetically.
+
+    Blow up if chan_str has duplicate channel names or garbage therein.
     """
     consumed = 0
     present: list[str] = []
+    # just loop through each channel in INDIVIDUAL_CHANS and for each one
+    # found in chan_str, put it into the 'present' list. afterward, make sure we
+    # exhausted all characters in chan_str (otherwise, there are either 
+    # duplicate channels or there's garbage in chan_str). finally, sort
+    # the 'present' list, make it into one long string and return it
     for candidate in INDIVIDUAL_CHANS:
         if candidate in chan_str:
             consumed += len(candidate)
